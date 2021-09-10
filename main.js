e3dce6b227e292a331a8def67541eab9851243d3
nghia <daovannghia1392001@hia1392001gmail.com>
2021-09-11 01:46:02 +0700

    update

--- main.js
+++ main.js
@@ -2,11 +2,13 @@
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
 
+const slides =$$('.slide-move__item')
 const slideUp = $('.slide-up')
 const slideBack = $('.slide-back')
 const widthSlide = $('.slide').clientWidth;
 const slideMove =  $('.slide-move')
 const maxSlide = widthSlide*2
+
 console.log(maxSlide)
 var up = 0
 
@@ -14,24 +16,58 @@ var up = 0
 function Up (){
     if(up < maxSlide){
         up += widthSlide
-    }else{
+    }
+    else{
         up=0
     }
-    slideMove.style.marginLeft = '-' + up + 'px'
+    // slideMove.style.transform =`translate3d(-${up}px, 0px, 0px)`
+
+    slides.forEach((tab,index) => {
+        if(up === widthSlide){
+            $('.slide-move__item.active').classList.remove('active')
+            slides[1].classList.add('active')
+        }if(up===maxSlide){
+            $('.slide-move__item.active').classList.remove('active')
+            slides[2].classList.add('active')
+
+        }if(up===0){
+            $('.slide-move__item.active').classList.remove('active')
+            slides[0].classList.add('active')
+        }
+    })
+
+    $('.slide-move__item.active').style.animation = `Slider ease 0.2s`
 }
 
 function Back (){
     if(up > 0){
         up -= widthSlide
 
-    }else{
+    }
+    else{
         up = maxSlide
     }
-    slideMove.style.marginLeft = '-' + up + 'px'
+    // slideMove.style.transform = `translate3d(-${up}px, 0px, 0px)`
+    slides.forEach((tab,index) => {
+        if(up === widthSlide){
+            $('.slide-move__item.active').classList.remove('active')
+            slides[1].classList.add('active')
+        }if(up===maxSlide){
+            $('.slide-move__item.active').classList.remove('active')
+            slides[2].classList.add('active')
+
+        }if(up===0){
+            $('.slide-move__item.active').classList.remove('active')
+            slides[0].classList.add('active')
+        }
+    })
+    $('.slide-move__item.active').style.animation = `SliderTT ease 0.2s`
+    
 }
+
 // Sau 3s chuển slide
 setInterval(function(){
-    Up()
+    Up ()
 },4000)
 
 slideUp.onclick = function(){
